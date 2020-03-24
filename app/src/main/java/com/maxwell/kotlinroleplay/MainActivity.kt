68aa6e3d7c4b630832c0270fbaa3b30245963ae6
Maximiliano Burgos <maxib2007@gmail.com>
2020-03-24 14:27:22 -0300

    enemies on json, loaded with gson

--- app/src/main/java/com/maxwell/kotlinroleplay/MainActivity.kt
+++ app/src/main/java/com/maxwell/kotlinroleplay/MainActivity.kt
@@ -8,27 +8,32 @@ import com.maxwell.kotlinroleplay.entities.Enemy
 import com.maxwell.kotlinroleplay.entities.Player
 import com.maxwell.kotlinroleplay.entities.Type
 import com.maxwell.kotlinroleplay.events.OnBattleEvents
+import com.maxwell.kotlinroleplay.utils.Utils
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() , OnBattleEvents {
     lateinit var battle:Battle
     lateinit var hero:Player
-    lateinit var slime:Enemy
+    lateinit var enemy:Enemy
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         hero = Player("Max")
-        slime = Enemy("Slime", 10, 5,10, 1, 2, 15)
+
+        val enemies = Utils().getEnemiesFromJson(this)
+
+        enemy = enemies[Random.nextInt(0, enemies.size - 1)]
 
         tvHeroName.text = "Hero name: ${hero.name}"
-        tvEnemyName.text = "Enemy name: ${slime.name}"
+        tvEnemyName.text = "Enemy name: ${enemy.name}"
 
         tvHeroHP.text = "Hero HP: ${hero.hp}"
-        tvEnemyHP.text = "Enemy HP: ${slime.hp}"
+        tvEnemyHP.text = "Enemy HP: ${enemy.hp}"
 
-        battle = Battle(hero, slime, this)
+        battle = Battle(hero, enemy, this)
 
         btPlayerAtq.setOnClickListener {
             battle.attack(Type.PLAYER)
@@ -41,19 +46,19 @@ class MainActivity : AppCompatActivity() , OnBattleEvents {
 
     override fun onDamage(entityType: Type, damage: Int) {
         if(entityType == Type.PLAYER){
-            Toast.makeText(this, "${hero.name} damage ${slime.name} by $damage", Toast.LENGTH_SHORT).show()
-            tvEnemyHP.text = "Enemy HP: ${slime.hp}"
+            Toast.makeText(this, "${hero.name} damage ${enemy.name} by $damage", Toast.LENGTH_SHORT).show()
+            tvEnemyHP.text = "Enemy HP: ${enemy.hp}"
         } else {
-            Toast.makeText(this, "${slime.name} damage ${hero.name} by $damage", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "${enemy.name} damage ${hero.name} by $damage", Toast.LENGTH_SHORT).show()
             tvHeroHP.text = "Hero HP: ${hero.hp}"
         }
     }
 
     override fun onResist(entityType: Type) {
         if(entityType == Type.PLAYER){
-            Toast.makeText(this, "${hero.name} resist the attack of ${slime.name}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "${hero.name} resist the attack of ${enemy.name}", Toast.LENGTH_SHORT).show()
         } else {
-            Toast.makeText(this, "${slime.name} resist the attack of ${hero.name}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "${enemy.name} resist the attack of ${hero.name}", Toast.LENGTH_SHORT).show()
         }
     }
 
---
+++ app/src/main/java/com/maxwell/kotlinroleplay/utils/Utils.kt
@@ -0,0 +1,42 @@
+package com.maxwell.kotlinroleplay.utils
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.maxwell.kotlinroleplay.R
+import com.maxwell.kotlinroleplay.entities.Enemy
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
+
+
+class Utils() {
+
+    fun getEnemiesFromJson(context:Context):ArrayList<Enemy>{
+        val inputStream: InputStream = context.resources.openRawResource(R.raw.enemies)
+        val jsonString = readTextFile(inputStream)
+
+        return Gson().fromJson<ArrayList<Enemy>>(jsonString)
+    }
+
+    private fun readTextFile(inputStream: InputStream):String{
+        val outputStream = ByteArrayOutputStream()
+
+        val buf = ByteArray(1024)
+        var len: Int
+
+        try {
+            while (inputStream.read(buf).also { len = it } != -1) {
+                outputStream.write(buf, 0, len)
+            }
+            outputStream.close()
+            inputStream.close()
+        } catch (e: IOException) {
+
+        }
+
+        return outputStream.toString()
+    }
+
+    inline fun <reified T> Gson.fromJson(json: String) = fromJson<T>(json, object: TypeToken<T>() {}.type)
+}
